8e86f77f77059ebd8fd7d1c11264013a9d388399
McDeid <Samuel.McDeid@dnr.iowa.gov>
2019-09-25 16:41:22 -0500

    Add test/test.py

--- gdbcat/cat.py
+++ gdbcat/cat.py
@@ -5,9 +5,12 @@ Created on Wed Sep 25 15:29:51 2019
 @author: smcdeid
 """
 
-#import os
+import os
 #import arcpy
 
 class Gdbcat:
     def __init__(self, name):
         self.name = name
+        
+        if not name:
+            self.name = 'blah blah blah'
